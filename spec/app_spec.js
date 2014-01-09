b1bf8f14f7c8da3a6d86668028e654b2413180da
Sean Copenhaver <sean.copenhaver@gmail.com>
2014-01-09 16:16:14 -0500

    basic project setup with live reloading dev server, es6 modules, less, and testem with mocha+chai specs

---
+++ spec/app_spec.js
@@ -0,0 +1,22 @@
+/*jshint expr: true */
+/*globals
+  describe beforeEach afterEach it
+  App visit
+ */
+var expect = chai.expect;
+
+describe('mocha with ember testing', function () {
+"use strict";
+
+    beforeEach(function () {
+        visit('/');
+    });
+
+    afterEach(function () {
+        App.reset();
+    });
+
+    it('renders and can use ember test helpers', function () {
+        expect(find('.jumbotron', '#ember-test-fixture')).to.exist;
+    });
+});
